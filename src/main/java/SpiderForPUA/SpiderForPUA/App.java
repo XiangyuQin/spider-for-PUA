88d55055fd8e5498e2407937acedafdffb6a9d98
xjmajiaoyu@sina.com <448230305@qq.com>
2015-10-24 20:59:48 +0800

    配置log4j 配置mybatis-generator

--- src/main/java/SpiderForPUA/SpiderForPUA/App.java
+++ src/main/java/SpiderForPUA/SpiderForPUA/App.java
@@ -1,13 +1,21 @@
 package SpiderForPUA.SpiderForPUA;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	Logger logger = LoggerFactory.getLogger(App.class);
+	public App(){
+		logger.info("haha");
+	}
     public static void main( String[] args )
     {
         System.out.println( "this is a spider" );
+        App app = new App();
+        
+        
     }
 }
